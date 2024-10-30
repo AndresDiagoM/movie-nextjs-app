8dc4041c4840e3e0645a20e73c598769842b2266
Andres Felipe Diago Matta <andresdiag@unicauca.edu.co>
2024-10-30 11:59:03 -0500

    :bricks: Folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // import styles from "./page.module.css";
 // import Image from "next/image";
-import WelcomePage from "./pages/welcome";
+import WelcomePage from "app/pages/welcome";
 
 export default function Home() {
 	return (
