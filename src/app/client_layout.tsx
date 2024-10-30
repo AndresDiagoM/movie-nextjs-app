27c69f140cab845a806ca9a1477361144a3e7e2d
Andres Felipe Diago Matta <andresdiag@unicauca.edu.co>
2024-10-30 09:07:09 -0500

    :art: Layout with Navigation and background

--- src/app/client_layout.tsx
+++ src/app/client_layout.tsx
@@ -24,7 +24,7 @@ export default function ClientLayout({
 	children: React.ReactNode;
 }) {
 	const pathname = usePathname();
-	const isHomePage = pathname === "/home";
+	const isHomePage = pathname === "/home" || pathname === "/" ? true : false;
 
 	return (
 		<div
@@ -38,48 +38,50 @@ export default function ClientLayout({
 			{/* Overlay for darker background */}
 			<div className="absolute inset-0 bg-black opacity-70"></div>
 
-			{/* Content */}
-			<div className="relative z-10 text-center text-white px-4 w-full h-full">
-				<header>
-					<nav
-						className={`flex justify-between items-center p-6 w-full max-w-6xl mx-auto`}
-					>
-						<h1 className="text-xl font-bold">Sudo-Flix</h1>
-						<ul className="flex space-x-6">
-							<li>
-								<Link href="/home" className="hover:underline">
-									Home
-								</Link>
-							</li>
-							<li>
-								<Link href="/movies" className="hover:underline">
-									Movies
-								</Link>
-							</li>
-							<li>
-								<Link href="/series" className="hover:underline">
-									Series
-								</Link>
-							</li>
-							<li>
-								<Link href="/trending" className="hover:underline">
-									Trending
-								</Link>
-							</li>
-						</ul>
-						<div className="flex space-x-4 items-center">
-							<a
-								href="#blocks"
-								className="flex ml-4 bg-white text-black py-2 px-4 rounded-full hover:bg-gray-200"
-							>
-								Login
-								<FaSignInAlt className="ml-2 w-full py-1" size="24px" />
-							</a>
-						</div>
-					</nav>
-				</header>
-				{children}
-			</div>
+			{/* Navigation */}
+			<header className="relative z-10 text-center text-white">
+				<nav
+					className={`flex justify-between items-center p-6 w-full max-w-6xl mx-auto`}
+				>
+					<Link href="/">
+						<h1 className="text-xl font-bold cursor-pointer">Sudo-Flix</h1>
+					</Link>
+					<ul className="flex space-x-6">
+						<li>
+							<Link href="/home" className="hover:underline">
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link href="/movies" className="hover:underline">
+								Movies
+							</Link>
+						</li>
+						<li>
+							<Link href="/series" className="hover:underline">
+								Series
+							</Link>
+						</li>
+						<li>
+							<Link href="/trending" className="hover:underline">
+								Trending
+							</Link>
+						</li>
+					</ul>
+					<div className="flex space-x-4 items-center">
+						<a
+							href="#blocks"
+							className="flex ml-4 bg-white text-black py-2 px-4 rounded-full hover:bg-gray-200"
+						>
+							Login
+							<FaSignInAlt className="ml-2 w-full py-1" size="24px" />
+						</a>
+					</div>
+				</nav>
+			</header>
+
+			{/* Main Content */}
+			<main className="relative z-10 flex-grow flex flex-col">{children}</main>
 		</div>
 	);
 }
