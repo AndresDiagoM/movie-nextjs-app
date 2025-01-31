26f161c49bf37ebc273ad09b37d4b9e39de10761
Andres Felipe Diago Matta <andresdiag@unicauca.edu.co>
2025-01-31 17:07:19 -0500

    :bug: Fix(image): vercel image optimization Optimization Costs in Vercel with Next.js

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,18 +1,40 @@
 import { prisma } from "app/libs/db";
+import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 
 async function POST(request) {
 	const data = await request.json();
 
-	const user = await prisma.user.create({
-		data: {
-			username: data.username,
-			email: data.email,
-			password: data.password,
-		},
-	});
+	try {
 
-	return NextResponse.json({ message: "Register route", user });
+		// Check if the user already exists
+		const userExists = await prisma.user.findUnique({
+			where: {
+                email: data.email,
+            },
+		});
+
+		if (userExists) {
+            console.log("User already exists");
+            return NextResponse.json({ message: "User already exists" }, { status: 409 });
+        }
+
+		const user = await prisma.user.create({
+			data: {
+				username: data.username,
+				email: data.email,
+				password: await bcrypt.hash(data.password, 10),
+			},
+		});
+
+		// const { password, ...userWithoutPassword } = user;
+		delete user.password;
+	
+		return NextResponse.json({ message: "Register route", user });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({ message: "An error occurred" }, { status: 500 });
+	}
 }
 
 export { POST };
